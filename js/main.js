15ae14a7bb581b8d58e59a0bce93a79bb33d6d29
Tanner Wilson <tw113@byui.edu>
2022-02-12 21:51:05 -0800

    todo list

--- js/main.js
+++ js/main.js
@@ -18,6 +18,10 @@ const links = [
   {
     label: 'Week 5 practice project',
     url: 'week5/index.html',
+  },
+  {
+    label: 'Week 6 midterm Todo List',
+    url: 'week6/index.html',
   }
 ];
 

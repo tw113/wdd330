9efe73c2680ee580b542f4e82e3b80c6e1f93778
Tanner Wilson <tw113@byui.edu>
2022-02-12 21:50:02 -0800

    todo app

--- js/ToDos.js
+++ js/ToDos.js
@@ -8,21 +8,128 @@ class Todo {
     this.complete = !this.complete;
   }
 }
-todoList = [];
 
-function refreshTodos() {
-  var todoListEl = document.getElementById('todo-list');
-  var i = 0;
-  todoListEl.innerHTML = '';
+let todoList = [];
 
+refreshTodos();
+
+function refreshTodos(filter = 'all') {
+  let theList = getListFromSessionStorage();
   const taskCountEl = document.getElementById('task-count');
-  taskCountEl.innerText = this.todoList.length + ' Tasks';
+  taskCountEl.innerText = theList.length + ' Tasks';
 
-  this.todoList.forEach((item) => {
+  switch (filter) {
+    case 'active':
+      theList = theList.filter(task => task.complete == false)
+      break;
+    case 'complete':
+      theList = theList.filter(task => task.complete == true)
+      break;
+    default:
+      break;
+  }
+
+  buildTodoListHTML(theList);
+}
+
+function addTodo() {
+  const theList = getListFromSessionStorage();
+
+  const newItem = new Todo(
+    Math.random() * 10,
+    document.getElementById('add-todo-box').value
+  );
+
+  theList.push(newItem);
+  addListToSessionStorage(theList);
+
+  refreshTodos();
+}
+
+function removeTodo(event) {
+  const theList = getListFromSessionStorage();
+  theList.splice(event.target.value, 1);
+  sessionStorage.setItem('tasks', JSON.stringify(theList));
+  refreshTodos();
+}
+
+function filterAll() {
+  const button = document.getElementById('btn-all')
+  const button2 = document.getElementById('btn-active')
+  const button3 = document.getElementById('btn-complete')
+  button.classList.add('active'); 
+  button2.classList.remove('active'); 
+  button3.classList.remove('active'); 
+  refreshTodos('all') 
+}
+
+function filterActive() { 
+  const button = document.getElementById('btn-active')
+  const button2 = document.getElementById('btn-all')
+  const button3 = document.getElementById('btn-complete')
+  button.classList.add('active'); 
+  button2.classList.remove('active'); 
+  button3.classList.remove('active'); 
+  refreshTodos('active') 
+}
+
+function filterComplete() { 
+  const button = document.getElementById('btn-complete')
+  const button2 = document.getElementById('btn-active')
+  const button3 = document.getElementById('btn-all')
+  button.classList.add('active'); 
+  button2.classList.remove('active'); 
+  button3.classList.remove('active'); 
+  refreshTodos('complete') 
+}
+
+function getListFromSessionStorage() {
+  const sessionItem = sessionStorage.getItem('tasks');
+  return JSON.parse(sessionItem);
+}
+
+function addListToSessionStorage(theList) {
+  sessionStorage.setItem('tasks', JSON.stringify(theList));
+}
+
+function toggleChecked(event) {
+  let theList = getListFromSessionStorage();
+
+  console.log(theList);
+
+  const taskIndex = theList.findIndex((task) => task.id == event.target.id);
+
+  theList[taskIndex].complete = !theList[taskIndex].complete;
+
+  addListToSessionStorage(theList);
+
+  refreshTodos();
+}
+
+function buildTodoListHTML(theList) {
+  const todoListEl = document.getElementById('todo-list');
+  todoListEl.innerHTML = '';
+  let i = 0;
+
+  theList.forEach((item) => {
     const newTodoEl = document.createElement('div');
     newTodoEl.className = 'todo-item';
-    newTodoEl.innerHTML =
-      "<input type='checkbox'><h4>" + item.content + '</h4>';
+
+    if (item.complete) {
+      newTodoEl.innerHTML =
+        "<input id='" +
+        item.id +
+        "' type='checkbox' onchange='toggleChecked(event)' checked><h4>" +
+        item.content +
+        '</h4>';
+    } else {
+      newTodoEl.innerHTML =
+        "<input name='chk-complete' id='" +
+        item.id +
+        "' type='checkbox' onchange='toggleChecked(event)'><h4>" +
+        item.content +
+        '</h4>';
+    }
 
     const deleteTodoEl = document.createElement('button');
     deleteTodoEl.className = 'delete-todo-button';
@@ -32,25 +139,5 @@ function refreshTodos() {
 
     newTodoEl.appendChild(deleteTodoEl);
     todoListEl.appendChild(newTodoEl);
-
-    console.log(i);
   });
-}
-
-function addTodo() {
-  todoList.push(
-    new Todo(
-      this.todoList.length,
-      document.getElementById('add-todo-box').value
-    )
-  );
-
-  console.log(todoList);
-
-  refreshTodos();
-}
-
-function removeTodo(event) {
-  this.todoList.splice(event.target.value, 1);
-  refreshTodos();
 }
